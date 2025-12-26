27b374aa4b80da1f7be05683a24fb1b06cab1b24
simonthi <github@simonthiefes.de>
2025-12-26 23:08:47 +0100

    Init

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,133 +1,110 @@
-let currentValue = 0;
+let currentValue = 1;
 let counterEl = document.getElementById("counter");
 
-const ANIMATION_DURATION = 1000; // 1 Sekunde
+const ANIMATION_DURATION = 1000;
 
 const SWITCH_FRACTION = 0.5;
 const NUMBER_DELAY = ANIMATION_DURATION * SWITCH_FRACTION;
 
 let isAnimating = false;
+const eventSource = new EventSource('https://ntfy.sh/39c3-farbfab-test/sse');
+eventSource.onmessage = (e) => {
+    var data = JSON.parse(e.data);
+    console.log(data.message.split(";"));
+    data = data.message.split(";");
+    document.querySelector('.total-waiting').innerHTML= data[1] || 0;
+    document.querySelector('.waiting-time').innerHTML = new Date(parseInt(data[3]) * 1000).toISOString().slice(11, 19) || 0;
+    updateCounter("up", data);
+    //                document.querySelector('.running-number').innerHTML = data[1]  || 0;
+    var desks = JSON.parse(data[2]);
+    document.querySelector('.desk-index').innerHTML = desks.join(", ")  || 0;
+};
+
+
 
 
 function fitCounterFont() {
-  if (!counterEl) {
-    counterEl = document.getElementById("counter");
-  }
-  if (!counterEl) return;
+    if (!counterEl) {
+        counterEl = document.getElementById("counter");
+    }
+    if (!counterEl) return;
 
-  const HEIGHT_FACTOR = 1.2;
+    const HEIGHT_FACTOR = 1.2;
 
-  // Höhe der Box messen
-  const rect = counterEl.getBoundingClientRect();
-  const maxHeight = rect.height;
+    // Höhe der Box messen
+    const rect = counterEl.getBoundingClientRect();
+    const maxHeight = rect.height;
 
-  if (maxHeight <= 0) return;
+    if (maxHeight <= 0) return;
 
-  const newSize = Math.floor(maxHeight * HEIGHT_FACTOR);
+    const newSize = Math.floor(maxHeight * HEIGHT_FACTOR);
 
 
-  counterEl.style.fontSize = newSize + "px";
+    counterEl.style.fontSize = newSize + "px";
 }
 
 
-function updateCounter(direction) {
-  if (!counterEl) {
-    counterEl = document.getElementById("counter");
-  }
-  if (!counterEl) return;
+function updateCounter(direction, data) {
+    if (!counterEl) {
+        counterEl = document.getElementById("counter");
+    }
+    if (!counterEl) return;
 
 
-  if (isAnimating) return;
-  isAnimating = true;
+    if (isAnimating) return;
+    isAnimating = true;
 
-  const delta = direction === "up" ? 1 : -1;
-  const nextValue = currentValue + delta;
+    //    const delta = direction === "up" ? 1 : -1;
+    //    const nextValue = parseInt(data.waitingindex);
 
-  counterEl.classList.remove("animate-up", "animate-down");
+    counterEl.classList.remove("animate-up", "animate-down");
 
-  void counterEl.offsetWidth;
+    void counterEl.offsetWidth;
 
-  if (direction === "up") {
-    counterEl.classList.add("animate-up");
-  } else {
-    counterEl.classList.add("animate-down");
-  }
+    if (direction === "up") {
+        counterEl.classList.add("animate-up");
+    }
 
-  setTimeout(() => {
-    currentValue = nextValue;
-    counterEl.textContent = currentValue;
+    setTimeout(() => {
+        counterEl.textContent = parseInt(data[0]);
 
-    fitCounterFont();
-  }, NUMBER_DELAY);
+        fitCounterFont();
+    }, NUMBER_DELAY);
 
-  setTimeout(() => {
-    isAnimating = false;
-  }, ANIMATION_DURATION);
+    setTimeout(() => {
+        isAnimating = false;
+    }, ANIMATION_DURATION);
 }
 
-//Enter hoch, Leertaste runter
-window.addEventListener("keydown", (event) => {
-  // Enter → hochzählen
-  if (event.key === "Enter") {
-    updateCounter("up");
-    return;
-  }
 
-  if (event.key === " " || event.code === "Space") {
-    event.preventDefault();
-    updateCounter("down");
-  }
-});
 
 window.addEventListener("load", () => {
-  if (!counterEl) {
-    counterEl = document.getElementById("counter");
-  }
-  fitCounterFont();
+    if (!counterEl) {
+        counterEl = document.getElementById("counter");
+    }
+    fitCounterFont();
 
-  if (window.ResizeObserver && counterEl) {
-    const ro = new ResizeObserver(() => {
-      fitCounterFont();
+    if (window.ResizeObserver && counterEl) {
+        const ro = new ResizeObserver(() => {
+            fitCounterFont();
+        });
+        ro.observe(counterEl);
+    }
+
+    fetch('https://ntfy.sh/39c3-farbfab-test/json?poll=1&since=latest').then(response => {
+        return response.json();
+    }).then(data => {
+        data = data.message.split(";");
+        document.querySelector('.total-waiting').innerHTML= data[1] || 0;
+        document.querySelector('.waiting-time').innerHTML = new Date(parseInt(data[3]) * 1000).toISOString().slice(11, 19) || 0;
+        updateCounter("up", data);
+        var desks = JSON.parse(data[2]);
+        document.querySelector('.desk-index').innerHTML = desks.join(", ")  || 0;
+    }).catch(err => {
+        console.log(err);
     });
-    ro.observe(counterEl);
-  }
 });
 
 window.addEventListener("resize", () => {
-  fitCounterFont();
+    fitCounterFont();
 });
-
-const rootElement = document.documentElement;
-const toggleButton = document.getElementById("powercycle");
-
-// interner Zustand für wdth
-let isWide = false;
-
-function toggleColors() {
-  // aktuelle Werte der CSS-Variablen holen
-  const styles = getComputedStyle(rootElement);
-  const primary = styles.getPropertyValue("--primary-color").trim();
-  const secondary = styles.getPropertyValue("--secondary-color").trim();
-
-  // Farben vertauschen
-  rootElement.style.setProperty("--primary-color", secondary);
-  rootElement.style.setProperty("--secondary-color", primary);
-
-  // wdth des Buttons toggeln (100 ↔ 10)
-  if (toggleButton) {
-    if (!isWide) {
-      // breit
-      toggleButton.style.fontVariationSettings = ' "opsz"160, "wght" 100, "wdth" 120';
-      isWide = true;
-    } else {
-      // schmal
-      toggleButton.style.fontVariationSettings = '"opsz"160, "wght" 10, "wdth" 120';
-      isWide = false;
-    }
-  }
-}
-
-if (toggleButton) {
-  toggleButton.addEventListener("click", toggleColors);
-}
-
